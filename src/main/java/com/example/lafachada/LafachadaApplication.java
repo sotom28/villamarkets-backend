1c3f40500f6226c5015703ab34655ae944467a67
Rodolfo soto <rod.sotod@duocuc.cl>
2026-04-16 21:06:40 -0400

    Initial commit

---
+++ src/main/java/com/example/lafachada/LafachadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lafachada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LafachadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LafachadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lafachada/LafachadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lafachada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LafachadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
